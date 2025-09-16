539d86ba285c7288c3a91b2bc52d4624bca00793
Tony Chu <24074951@student.uwa.edu.au>
2025-09-16 17:29:45 +0800

    feat(display): add public screen display page
    based on feature branch completition.html refactoring

--- app/routes.py
+++ app/routes.py
@@ -40,9 +40,9 @@ def display():
 def display_index():
     return render_template('display/selection.html')
 
-# @display_bp.route('/competition')
-# def display_competition():
-#     return render_template('display/competition.html')
+@display_bp.route('/competition')
+def display_competition():
+    return render_template('display/competition.html')
 
 @display_bp.route('/datatable')
 def display_datatable():
