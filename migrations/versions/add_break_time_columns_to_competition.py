16f4c01b3698fad3608213fa3c20713f662ce16f
24085253 <24085253@student.uwa.edu.au>
2025-10-12 05:31:37 +0800

    fix: correct migration down_revision for break time columns
    Fixed migration file reference to use correct parent revision d36e2219443d.
    Manually applied schema changes to add breaktime_between_events and
    breaktime_between_flights columns to competition table.
    
    This resolves HTTP 500 error in /display/api/competition/<id>/flights-data endpoint.

--- migrations/versions/add_break_time_columns_to_competition.py
+++ migrations/versions/add_break_time_columns_to_competition.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'abc123def456'
-down_revision = 'dd1cdc0a9afe'
+down_revision = 'd36e2219443d'
 branch_labels = None
 depends_on = None
 
